76781544d7e7fcb6fa81b40810f0fc0834caa63a
Eimantas <eimantas.stonys@gmail.com>
2015-09-11 13:26:37 +0300

    Add basic template, static files

--- autumnbreeze/settings.py
+++ autumnbreeze/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'autumnbreeze',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'autumnbreeze.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'templates',
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,6 +70,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'autumnbreeze.wsgi.application'
 
